ac5f359d4524d1d6c10e7f155326d4540f37dda6
senior67 <senior67@users.noreply.github.com>
2020-05-15 12:36:06 +0300

    Updates

---
+++ precache-manifest.802bf001b8047a90d479a06232351869.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78ad02ea7328e156471561e0767eb5aa",
+    "url": "/rfsvak/index.html"
+  },
+  {
+    "revision": "dc3099e7fbc3e78ce817",
+    "url": "/rfsvak/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "c66839b531a36f1c800c",
+    "url": "/rfsvak/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "dc3099e7fbc3e78ce817",
+    "url": "/rfsvak/static/js/2.fc6a7c9b.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/rfsvak/static/js/2.fc6a7c9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c66839b531a36f1c800c",
+    "url": "/rfsvak/static/js/main.8eb044e9.chunk.js"
+  },
+  {
+    "revision": "337c1ab158b072d84bea",
+    "url": "/rfsvak/static/js/runtime-main.2ae04bb3.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/rfsvak/static/media/persik.4e1ec840.png"
+  }
+]);
