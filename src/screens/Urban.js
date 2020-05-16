35a623c71f2c743027332034c5a1ddc09b311016
senior67 <senior67@users.noreply.github.com>
2020-05-16 16:30:55 +0300

    top

--- src/screens/Urban.js
+++ src/screens/Urban.js
@@ -73,8 +73,88 @@ export default function Urban(props) {
                     <CardGrid>
                         <Card size="l" mode="shadow" style={{ marginTop: 10 }}>
                             <Div>
-                                <Headline weight="regular" style={{ marginBottom: 0 }}>Airbus А320-200 — одна из модификаций популярного Airbus A320. Основное отличие от предыдущей версии — увеличенный объём топливных баков и множество технических нововведений, касающихся двигательной части.</Headline>
-                                <img src={require('./../img/A320.jpg')} alt="photo A320" width="100%" />
+                                <Headline weight="regular" style={{ marginBottom: 0 }}>Airbus А320-200 — одна из модификаций популярного Airbus A320. Основное отличие от предыдущей версии — увеличенный объём топливных баков и множество технических нововведений, касающихся двигательной части.<br /></Headline>
+                                <img src={require('./../img/A320.jpg')} alt="photo A320" width="100%"/>
+                            </Div>
+                        </Card>
+                    </CardGrid>
+                </Group>
+                <Group separator="hide" header={<Header mode="secondary">Airbus A330-300</Header>}>
+                    <CardGrid>
+                        <Card size="l" mode="shadow" style={{ marginTop: 10 }}>
+                            <Div>
+                                <Headline weight="regular" style={{ marginBottom: 0 }}>Эксплуатация авиалайнера Airbus А330-300 (Аэробус А330-300) началась в 1994 году. Выпуск этого дальнемагистрального, двухдвигательного самолета продолжается и в настоящее время. За основу конструкции взят Аэробус А330, созданный во второй половине 1980-х годов. Почти одновременно была создана модификация А330-200. Ее отличие – укороченный фюзеляж.<br /></Headline>
+                                <img src={require('./../img/A330.jpg')} alt="photo A330" width="100%"/>
+                            </Div>
+                        </Card>
+                    </CardGrid>
+                </Group>
+                <Group separator="hide" header={<Header mode="secondary">Airbus А340-200</Header>}>
+                    <CardGrid>
+                        <Card size="l" mode="shadow" style={{ marginTop: 10 }}>
+                            <Div>
+                                <Headline weight="regular" style={{ marginBottom: 0 }}>Четырехдвигательный, широкофюзеляжный Airbus A340-200, это пассажирский самолет, разработанный западноевропейским консорциумом «Airbus S.A.S.», как часть семейства дальнемагистральных авиалайнеров А340. Данный самолет является самой короткой машиной в этом семейства. Длина А340-200 составляет 59,4 метра, что на 4,29 метров короче своей старшей модели А340-300.<br /></Headline>
+                                <img src={require('./../img/A340.jpg')} alt="photo A340" width="100%" />
+                            </Div>
+                        </Card>
+                    </CardGrid>
+                </Group>
+                <Group separator="hide" header={<Header mode="secondary">Airbus A380-800</Header>}>
+                    <CardGrid>
+                        <Card size="l" mode="shadow" style={{ marginTop: 10 }}>
+                            <Div>
+                                <Headline weight="regular" style={{ marginBottom: 0 }}>В мире существует огромное количество различных авиалайнеров, но Airbus A380-800 – это тот самолет, на который действительно стоит обратить внимание. Подобных лайнеров на планете не очень много. Дело в том, что этот является самым большим пассажирским широкофюзеляжным летательным аппаратом, оснащенным двумя палубами. <br /></Headline>
+                                <img src={require('./../img/A380.jpg')} alt="photo A320" width="100%" />
+                            </Div>
+                        </Card>
+                    </CardGrid>
+                </Group>
+                <Group separator="hide" header={<Header mode="secondary">ATR-72</Header>}>
+                    <CardGrid>
+                        <Card size="l" mode="shadow" style={{ marginTop: 10 }}>
+                            <Div>
+                                <Headline weight="regular" style={{ marginBottom: 0 }}>ATR 72 — пассажирский двухмоторный турбовинтовой самолёт для среднемагистральных перелётов. Самолёт предназначен для перевозки до 74 пассажиров одного класса на расстояние до 1300 км и управляется двумя пилотами. Производитель — франко-итальянский концерн ATR.<br /></Headline>
+                                <img src={require('./../img/ATR72.jpg')} alt="photo A320" width="100%" />
+                            </Div>
+                        </Card>
+                    </CardGrid>
+                </Group>
+                <Group separator="hide" header={<Header mode="secondary">Boeing 737-800</Header>}>
+                    <CardGrid>
+                        <Card size="l" mode="shadow" style={{ marginTop: 10 }}>
+                            <Div>
+                                <Headline weight="regular" style={{ marginBottom: 0 }}>Boeing 737–800 — среднемагистральный пассажирский самолет, один из самых популярных американских лайнеров в парках мировых авиакомпаний, в том числе и чешских. Выпускается корпорацией Boeing с конца 1990-х годов.<br /></Headline>
+                                <img src={require('./../img/B737.jpg')} alt="photo A320" width="100%" />
+                            </Div>
+                        </Card>
+                    </CardGrid>
+                </Group>
+                <Group separator="hide" header={<Header mode="secondary">Boeing 787-8 DREAMLINER</Header>}>
+                    <CardGrid>
+                        <Card size="l" mode="shadow" style={{ marginTop: 10 }}>
+                            <Div>
+                                <Headline weight="regular" style={{ marginBottom: 0 }}>Boeing 787 Dreamliner — широкофюзеляжный двухдвигательный пассажирский самолёт, разработанный американской компанией Boeing. По вместимости и дальности полёта 787 сравним с Boeing 767. Boeing утверждает, что Dreamliner более экономичный, чем предыдущие разработки. Boeing 787 стал первым пассажирским самолётом компании, в конструкции которого широко использованы композитные материалы. <br /></Headline>
+                                <img src={require('./../img/B787.jpg')} alt="photo A320" width="100%" />
+                            </Div>
+                        </Card>
+                    </CardGrid>
+                </Group>
+                <Group separator="hide" header={<Header mode="secondary">BOMBARDIER CRJ900</Header>}>
+                    <CardGrid>
+                        <Card size="l" mode="shadow" style={{ marginTop: 10 }}>
+                            <Div>
+                                <Headline weight="regular" style={{ marginBottom: 0 }}>Bombardier CRJ900 представляет собой удлиненную версию модели CRJ-700, вместе с которой входит в семейство региональных самолетов CRJ. CRJ900 предназначен для обслуживания региональных маршрутов с повышенным пассажиропотоком.<br /></Headline>
+                                <img src={require('./../img/CRJ900.jpg')} alt="photo A320" width="100%" />
+                            </Div>
+                        </Card>
+                    </CardGrid>
+                </Group>
+                <Group separator="hide" header={<Header mode="secondary">TUPOLEV TU-154M</Header>}>
+                    <CardGrid>
+                        <Card size="l" mode="shadow" style={{ marginTop: 10 }}>
+                            <Div>
+                                <Headline weight="regular" style={{ marginBottom: 0 }}>Ту-154М (по кодификации НАТО: Careless — «Беззаботный», на сленге советских пилотов — «Полтинник», «Туполь», «Большая Тушка» или «Аврора») — советский трёхдвигательный реактивный пассажирский авиалайнер для авиалиний средней протяжённости, рассчитанный на перевозку 152—180 пассажиров. Разработан в 1960-х годах в СССР в ОКБ Туполева. <br /></Headline>
+                                <img src={require('./../img/TU154M.jpg')} alt="photo A320" width="100%" />
                             </Div>
                         </Card>
                     </CardGrid>
